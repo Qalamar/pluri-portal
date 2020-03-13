8a5a74a9ee021865553b1f5ec688a905bac3ef06
xorium <hamr34@gmail.com>
2020-03-13 17:20:21 +0100

    Users page & Fetching users from the API

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -15,17 +15,36 @@ import {
   IonCardContent,
   IonIcon,
   IonItem,
-  IonLabel
+  IonLabel,
+  IonChip,
+  IonText,
+  IonList,
+  IonButton
 } from "@ionic/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   calendarOutline,
   trophyOutline,
-  logoWebComponent
+  logoWebComponent,
+  linkOutline,
+  imageOutline
 } from "ionicons/icons";
 import Anime from "react-anime";
+import axios from "axios";
+import "./Accomplishements.css";
 
 const Users: React.FC = () => {
+  const [students, setstudents] = useState([]);
+
+  const getUsers = async () => {
+    let res = await axios.get("/students");
+    let data = res.data;
+    setstudents(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <IonPage>
       <IonHeader>
@@ -42,7 +61,6 @@ const Users: React.FC = () => {
         <Anime opacity={[0, 1]} duration={2000} easing="easeOutElastic">
           <IonGrid>
             <IonRow class="ion-align-items-center">
-              <IonCol></IonCol>
               <IonCol size="12">
                 <IonCard class="neum">
                   <IonCardHeader class="ion-text-center ion-padding">
@@ -52,12 +70,50 @@ const Users: React.FC = () => {
                   </IonCardHeader>
                   <IonCardContent>
                     <IonGrid>
-                      <IonRow></IonRow>
+                      <IonRow>
+                        {students.length === 0 ? (
+                          <div>Loading...</div>
+                        ) : (
+                          students.map((e: any, i) => {
+                            return (
+                              <IonCol
+                                size="12"
+                                sizeMd="3"
+                                class=" ion-text-center"
+                              >
+                                <IonCard
+                                  className="user"
+                                  class="shadow ion-text-center"
+                                >
+                                  <IonCardHeader></IonCardHeader>
+                                  <IonCardTitle
+                                    color="dark"
+                                    className="ion-padding"
+                                  >
+                                    <strong>
+                                      {e.first_name + " " + e.last_name}
+                                    </strong>
+                                  </IonCardTitle>
+                                  <IonChip outline={true} color="dark">
+                                    <IonLabel>Group: {e.group}</IonLabel>
+                                  </IonChip>
+                                  <IonChip color="dark" outline={true}>
+                                    <IonLabel>{e.class}</IonLabel>
+                                  </IonChip>
+
+                                  <IonCardContent>
+                                    <IonText>A simple user.</IonText>
+                                  </IonCardContent>
+                                </IonCard>
+                              </IonCol>
+                            );
+                          })
+                        )}
+                      </IonRow>
                     </IonGrid>
                   </IonCardContent>
                 </IonCard>
               </IonCol>
-              <IonCol></IonCol>
             </IonRow>
           </IonGrid>
         </Anime>
