12b61cff9a724097f8b7e802dc869ab2bd2b7c44
SouhilaGuendouzi <61289836+SouhilaGuendouzi@users.noreply.github.com>
2020-04-20 19:16:57 +0200

    Create Interfaces.tsx
    Interfaces Team+Student

---
+++ src/utils/Interfaces.tsx
@@ -0,0 +1,72 @@
+import React ,{useState} from "react";
+
+export interface Team {
+    id:number,
+    name:string,
+    readiness:boolean
+  } 
+export interface TeamInterface {
+  TeamIn:Team;
+}
+interface TeamState {
+    Team: Team;
+    setTeam: React.Dispatch<React.SetStateAction<Team>>;
+  }
+  
+  export  const useTeam = (overrides?: Partial<Team>): TeamState => {
+      const defaultPromo: Team= {
+        id:0,
+    name:"",
+    readiness:false,
+    
+    
+      };
+      const [Team, setTeam] = useState<Team>({
+    ...defaultPromo,
+    ...overrides,
+  }); 
+   return {Team, setTeam };
+    };
+
+ export interface Student{
+    id: number,
+    firstName:string,
+    lastName:string,
+   dateOfBirth:Date,
+    placeOfBirth:string,
+    email:string,
+    userName:string,
+    password:string,
+    promotion:string,
+    isLeader:boolean
+    team:string,
+    }
+export interface sTudent {
+    stuDent:Student,
+
+}
+export interface StudentState{
+  student:Student;
+  setStudent: React.Dispatch<React.SetStateAction<Student>>;
+}
+export  const useStudent = (overrides?: Partial<Student>): StudentState => {
+  const defaultStudent: Student= {
+    id:0,
+    firstName:"",
+    lastName:"",
+   dateOfBirth:new Date(),
+    placeOfBirth:"",
+    email:"",
+    userName:"",
+    password:"",
+    promotion:"",
+    isLeader:false,
+    team:"", 
+
+  };
+  const [student, setStudent] = useState<Student>({
+...defaultStudent,
+...overrides,
+}); 
+return {student, setStudent };
+};
