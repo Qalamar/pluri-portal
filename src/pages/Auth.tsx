726993ba54b6f7bb11545ac81ba861fe5b2b545c
Tariq Hamrit <contact@tariqhamrit.com>
2020-05-23 15:05:25 +0100

    Authentication

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -25,6 +25,8 @@ import {
   IonItemSliding,
   IonItemOptions,
   IonItemOption,
+  IonSelect,
+  IonSelectOption,
 } from "@ionic/react";
 import React, { useState } from "react";
 import Anime from "react-anime";
@@ -35,10 +37,23 @@ import {
   addCircleOutline,
   calendarOutline,
   removeCircleOutline,
+  personCircleOutline,
+  personOutline,
 } from "ionicons/icons";
 import axios from "axios";
 import NotificationArea from "../components/NotificationArea";
 import "./Auth.css";
+import { useForm, Controller } from "react-hook-form";
+
+let initialValues = {
+  rangeInfo: -100,
+  firstName: "",
+  lastName: "",
+  gender: "",
+  class: "",
+  email: "",
+  promo: "",
+};
 
 const Auth: React.FC = () => {
   const [showPopover, setShowPopover] = useState<{
@@ -50,6 +65,8 @@ const Auth: React.FC = () => {
   });
 
   const [isOpen, setisOpen] = useState(false);
+  const [role, setRole] = useState<string>("student");
+
   const showProfile = () => {
     axios.get("/employees?id=1").then(function (response) {
       let data = response.data;
@@ -62,23 +79,55 @@ const Auth: React.FC = () => {
   const handleImageChange = (e: any) => {
     setimage(e.target.files[0]);
   };
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(image);
-    let form_data = new FormData();
-    form_data.append("image", "Test");
-    let url = "http://localhost:3000/employees/";
-    axios
-      .post(url, form_data, {
-        headers: {
-          "content-type": "multipart/form-data",
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => console.log(err));
+
+  // const handleSubmit = (e: any) => {
+  //   e.preventDefault();
+  //   console.log(image);
+  //   let form_data = new FormData();
+  //   form_data.append("image", "Test");
+  //   let url = "http://localhost:3000/employees/";
+  //   axios
+  //     .post(url, form_data, {
+  //       headers: {
+  //         "content-type": "multipart/form-data",
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
+
+  const { control, handleSubmit, formState, reset, errors } = useForm({
+    defaultValues: { ...initialValues },
+    mode: "onChange",
+  });
+
+  const showError = (_fieldName: string) => {
+    let error = (errors as any)[_fieldName];
+    return error ? (
+      <div style={{ color: "red" }}>{error.message || "Field Is Required"}</div>
+    ) : null;
   };
+
+  const onSubmit = (data: any) => {
+    axios
+      .post("/students", {
+        first_name: data.firstName,
+        last_name: data.lastName.toUpperCase(),
+        class: data.class,
+        email: data.email,
+        gender: data.gender,
+        promo: data.promo,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -165,19 +214,96 @@ const Auth: React.FC = () => {
                       Auth
                     </IonTitle>
                   </IonCardHeader>
+
                   <IonCardContent class="ion-padding ion-text-center">
-                    <IonItem class="ion-margin ">
-                      <IonIcon slot="start" icon={mailOutline}></IonIcon>
-                      <IonInput placeholder="Email"></IonInput>
-                    </IonItem>
-                    <IonItem class="ion-margin">
-                      <IonIcon slot="start" icon={lockClosedOutline}></IonIcon>
-                      <IonInput
-                        type="password"
-                        placeholder="Password"
-                      ></IonInput>
-                    </IonItem>
-                    <IonButton color="dark" type="submit">
+                    <form
+                      onSubmit={handleSubmit(onSubmit)}
+                      style={{ padding: 38 }}
+                    >
+                      <IonItem>
+                        <IonIcon slot="start" icon={mailOutline}></IonIcon>
+                        <Controller
+                          as={IonInput}
+                          placeholder="Email"
+                          inputmode="email"
+                          control={control}
+                          onChangeName="onIonChange"
+                          onChange={([selected]) => {
+                            return selected.detail.value;
+                          }}
+                          name="email"
+                          rules={{
+                            required: true,
+                            pattern: {
+                              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                              message: "invalid email address",
+                            },
+                          }}
+                        />
+                      </IonItem>
+                      {showError("email")}
+                      <IonItem class="">
+                        <IonIcon
+                          slot="start"
+                          icon={personCircleOutline}
+                        ></IonIcon>
+
+                        <Controller
+                          as={IonInput}
+                          placeholder="First Name"
+                          className="firstCapital"
+                          control={control}
+                          onChangeName="onIonChange"
+                          onChange={([selected]) => {
+                            console.log("firstName", selected.detail.value);
+                            return selected.detail.value;
+                          }}
+                          name="firstName"
+                          rules={{
+                            required: true,
+                            minLength: {
+                              value: 4,
+                              message: "Must be 4 chars long",
+                            },
+                          }}
+                        />
+                      </IonItem>
+                      {showError("firstName")}
+                      <IonItem class="ion-margin-bottom">
+                        <IonIcon slot="start" icon={personOutline}></IonIcon>
+                        <IonLabel>Role</IonLabel>
+                        <Controller
+                          as={
+                            <IonSelect>
+                              <IonSelectOption value="student">
+                                Student
+                              </IonSelectOption>
+                              <IonSelectOption value="teacher">
+                                Techer
+                              </IonSelectOption>
+                              <IonSelectOption value="other">
+                                Other
+                              </IonSelectOption>
+                            </IonSelect>
+                          }
+                          control={control}
+                          onChangeName="onIonChange"
+                          onChange={([selected]) => {
+                            console.log(selected.detail.value);
+                            setRole(selected.detail.value);
+                            return selected.detail.value;
+                          }}
+                          name="gender"
+                          rules={{ required: true }}
+                        />
+                      </IonItem>
+                    </form>
+
+                    <IonButton
+                      color="dark"
+                      type="submit"
+                      disabled={formState.isValid === false}
+                    >
                       Login
                     </IonButton>
                   </IonCardContent>
