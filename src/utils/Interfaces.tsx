9c79009117e68a2fadc89fb146a1fc59065f994d
SouhilaGuendouzi <61289836+SouhilaGuendouzi@users.noreply.github.com>
2020-05-30 23:59:41 +0200

    Update Interfaces.tsx

--- src/utils/Interfaces.tsx
+++ src/utils/Interfaces.tsx
@@ -1,82 +1,111 @@
 import React, { useState } from "react";
 
 export interface Team {
-  id: number,
-  name: string,
-  readiness: boolean
-}
+    id:number,
+    name:string,
+    readiness:boolean,
+    project:number
+  } 
 export interface TeamInterface {
-  TeamIn: Team;
+  TeamIn:Team;
 }
-export interface NotificationInterface {
-  Desciption: string,
-  Icon: string
-}
-
 interface TeamState {
-  Team: Team;
-  setTeam: React.Dispatch<React.SetStateAction<Team>>;
-}
-
-export const useTeam = (overrides?: Partial<Team>): TeamState => {
-  const defaultPromo: Team = {
-    id: 0,
-    name: "",
-    readiness: false,
-
-
-  };
-  const [Team, setTeam] = useState<Team>({
+    Team: Team;
+    setTeam: React.Dispatch<React.SetStateAction<Team>>;
+  }
+  
+  export  const useTeam = (overrides?: Partial<Team>): TeamState => {
+      const defaultPromo: Team= {
+        id:0,
+    name:"",
+    readiness:false,
+    project:0,
+    
+      };
+      const [Team, setTeam] = useState<Team>({
     ...defaultPromo,
     ...overrides,
-  });
-  return { Team, setTeam };
-};
+  }); 
+   return {Team, setTeam };
+    };
 
-export interface Student {
-  id: number,
-  firstName: string,
-  lastName: string,
-  dateOfBirth: Date,
-  placeOfBirth: string,
-  email: string,
-  userName: string,
-  password: string,
-  promotion: string,
-  currentYear: string,
-  isLeader: boolean
-  team: string,
-}
+ export interface Student{
+    id: number,
+    firstName:string,
+    lastName:string,
+   dateOfBirth:Date,
+    placeOfBirth:string,
+    email:string,
+    userName:string,
+    password:string,
+    promotion:number
+    isLeader:boolean,
+    note:number
+              }
 export interface sTudent {
-  stuDent: Student,
+    student:Student,
 
 }
-export interface StudentState {
-  student: Student;
+export interface StudentState{
+  student:Student;
   setStudent: React.Dispatch<React.SetStateAction<Student>>;
 }
-export const useStudent = (overrides?: Partial<Student>): StudentState => {
-  const defaultStudent: Student = {
-    id: 0,
-    firstName: "",
-    lastName: "",
-    dateOfBirth: new Date(),
-    placeOfBirth: "",
-    email: "",
-    userName: "",
-    password: "",
-    promotion: "",
-    currentYear: '',
-    isLeader: false,
-    team: "",
-
+export  const useStudent = (overrides?: Partial<Student>): StudentState => {
+  const defaultStudent: Student= {
+    id:0,
+    firstName:"",
+    lastName:"",
+   dateOfBirth:new Date(),
+    placeOfBirth:"",
+    email:"",
+    userName:"",
+    password:"",
+    promotion:0,
+    isLeader:false,
+    note:0,
   };
   const [student, setStudent] = useState<Student>({
-    ...defaultStudent,
-    ...overrides,
-  });
-  return { student, setStudent };
+...defaultStudent,
+...overrides,
+}); 
+return {student, setStudent };
 };
+export interface studentSecure{ //for users/students
+  id: number,
+  firstName:string,
+  lastName:string,
+  promotion:number,
+  isLeader:boolean,
+  team:number,
+  }
+export interface secureStudent {
+  Sstudent:studentSecure,
+
+}
+export interface studentState{
+Sstudent:studentSecure;
+setSstudent: React.Dispatch<React.SetStateAction<studentSecure>>;
+}
+export  const useSecureStudent = (overrides?: Partial<studentSecure>): studentState => {
+const defaultStudent: studentSecure= { 
+  id:0,
+  firstName:"",
+  lastName:"",
+  promotion:0,
+  isLeader:false,
+  team:0, 
+
+};
+const [Sstudent, setSstudent] = useState<studentSecure>({
+...defaultStudent,
+...overrides,
+}); 
+return {Sstudent, setSstudent };
+};
+
+
+
+
 export interface Invite {
   id:number,
   sender:number;
