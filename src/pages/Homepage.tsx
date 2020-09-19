9a20f37fee5045aabfb701361b4ab3c273c56404
Tariq Hamrit <contact@tariqhamrit.com>
2020-09-19 14:11:24 +0100

    Added Charts

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -12,13 +12,15 @@ import {
   IonModal,
   IonPage,
   IonRow,
-  IonTitle,
+  IonTitle
 } from "@ionic/react";
-import { closeOutline, logoIonic, logoReact, server } from "ionicons/icons";
+import { closeOutline, logoIonic, logoReact, newspaperOutline, server } from "ionicons/icons";
 import React, { useEffect, useState } from "react";
 import Toolbar from "../components/Toolbar";
 import "../forms/promotion/PromoForm.css";
 import { store } from "../utils/Store";
+import { ResponsivePie } from "@nivo/pie";
+
 import "./Auth.css";
 
 const Homepage: React.FC = () => {
@@ -27,6 +29,22 @@ const Homepage: React.FC = () => {
     store.page = "about";
     if (store.isAuth.access === "2") setShowModal(true);
   }, []);
+
+  const data = [
+    {
+      id: "Correct",
+      label: "Correct",
+      value: 6,
+      color: "hsl(134, 70%, 50%)",
+    },
+    {
+      id: "Wrong",
+      label: "Wrong",
+      value: 4,
+      color: "hsl(127, 70%, 50%)",
+    },
+  ];
+
   return (
     <IonPage>
       <Toolbar page={"About"} />
@@ -56,19 +74,152 @@ const Homepage: React.FC = () => {
       <IonContent className="showcase">
         <IonGrid>
           <IonRow class="ion-align-items-center ">
-            <IonCol></IonCol>
-            <IonCol size="12" sizeMd="7" sizeXl="5">
-              <IonCard class="ion-text-center shadow holder">
-                <IonCardHeader>
-                  <IonTitle color="dark" class="title">
-                    Pluri Portal
-                  </IonTitle>
-                </IonCardHeader>
+            <IonCol size="12" sizeXl="8" class='ion-padding'>
+              <IonCard className='home-cart holder ion-padding'>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonTitle>Summary</IonTitle>
+                      <IonCard className='summary'>
 
-                <IonCardContent class="ion-padding ion-text-center">
+                      </IonCard>
+                    </IonCol>
+                    <IonCol>
+                      <div style={{ height: "250px" }}>
+                        <ResponsivePie
+                          data={data}
+                          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                          innerRadius={0.5}
+                          padAngle={0.7}
+                          cornerRadius={3}
+                          colors={{ scheme: 'nivo' }}
+                          borderWidth={1}
+                          borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+                          radialLabelsSkipAngle={10}
+                          radialLabelsTextXOffset={6}
+                          radialLabelsTextColor="#333333"
+                          radialLabelsLinkOffset={0}
+                          radialLabelsLinkDiagonalLength={16}
+                          radialLabelsLinkHorizontalLength={24}
+                          radialLabelsLinkStrokeWidth={1}
+                          radialLabelsLinkColor={{ from: 'color' }}
+                          slicesLabelsSkipAngle={10}
+                          slicesLabelsTextColor="#333333"
+                          animate={true}
+                          motionStiffness={90}
+                          motionDamping={15}
+                          defs={[
+                            {
+                              id: 'dots',
+                              type: 'patternDots',
+                              background: 'inherit',
+                              color: 'rgba(255, 255, 255, 0.3)',
+                              size: 4,
+                              padding: 1,
+                              stagger: true
+                            },
+                            {
+                              id: 'lines',
+                              type: 'patternLines',
+                              background: 'inherit',
+                              color: 'rgba(255, 255, 255, 0.3)',
+                              rotation: -45,
+                              lineWidth: 6,
+                              spacing: 10
+                            }
+                          ]}
+                          fill={[
+                            {
+                              match: {
+                                id: 'ruby'
+                              },
+                              id: 'dots'
+                            },
+                            {
+                              match: {
+                                id: 'c'
+                              },
+                              id: 'dots'
+                            },
+                            {
+                              match: {
+                                id: 'go'
+                              },
+                              id: 'dots'
+                            },
+                            {
+                              match: {
+                                id: 'python'
+                              },
+                              id: 'dots'
+                            },
+                            {
+                              match: {
+                                id: 'scala'
+                              },
+                              id: 'lines'
+                            },
+                            {
+                              match: {
+                                id: 'lisp'
+                              },
+                              id: 'lines'
+                            },
+                            {
+                              match: {
+                                id: 'elixir'
+                              },
+                              id: 'lines'
+                            },
+                            {
+                              match: {
+                                id: 'javascript'
+                              },
+                              id: 'lines'
+                            }
+                          ]}
+                          legends={[
+                            {
+                              anchor: 'bottom',
+                              direction: 'row',
+                              translateY: 56,
+                              itemWidth: 100,
+                              itemHeight: 18,
+                              itemTextColor: '#999',
+                              symbolSize: 18,
+                              symbolShape: 'circle',
+                              effects: [
+                                {
+                                  on: 'hover',
+                                  style: {
+                                    itemTextColor: '#000'
+                                  }
+                                }
+                              ]
+                            }
+                          ]}
+                        />
+                      </div>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonCard>
+            </IonCol>
+            <IonCol size="12" sizeMd="4" sizeXl="4" class='ion-padding'>
+              <IonCard class="ion-text-center holder home-cart ion-padding">
+                <div>
+                  <IonIcon
+                    icon={newspaperOutline}
+                    class="ico"
+                    color="dark"
+                  />
+                </div>
+                <IonCardContent class=" ion-text-center">
                   <IonGrid>
-                    <IonRow class="ion-padding">
+                    <IonRow class="ion-margin-bottom">
                       <IonCol>
+                        <IonTitle color="dark" class="project">
+                          <strong>Pluri Portal</strong></IonTitle>
                         <IonLabel>
                           Pluri portal is an academic platform for teachers and
                           students , allowing project submission, preview and
@@ -79,7 +230,7 @@ const Homepage: React.FC = () => {
                     <IonLabel>
                       <strong>Technologies</strong>
                     </IonLabel>
-                    <IonRow class="ion-padding ion-justify-content-center">
+                    <IonRow class="ion-padding-horizontal ion-justify-content-center">
                       <IonCol>
                         <IonChip outline={false} color="danger">
                           <IonIcon icon={logoReact} />
@@ -95,11 +246,11 @@ const Homepage: React.FC = () => {
                         </IonChip>
                       </IonCol>
                     </IonRow>
+
                   </IonGrid>
                 </IonCardContent>
               </IonCard>
             </IonCol>
-            <IonCol></IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
